97550394c3b5dbac1afaaeac110f8645dbd5cf79
unknown <Riyanagwl@gmail.com>
2023-04-10 12:36:04 +0300

    Add views

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,16 @@
-// Entry point for the build script in your package.json
-import "@hotwired/turbo-rails"
-import "./controllers"
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Greeting from '../components/Greeting';
+const App = () => {
+  return (
+    <Router>
+      <div>
+        <Route exact path="/" component={Greeting} />
+      </div>
+    </Router>
+  );
+};
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render(<App />, document.getElementById('root'));
+});
